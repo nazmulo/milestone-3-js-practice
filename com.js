58a58b2304896f766f05034b3f45f61913b29f6c
nazmulo <itsnaazmul@gmail.com>
2026-08-17 00:10:42 +0600

    co0mparison hard core use

--- com.js
+++ com.js
@@ -1,11 +1,45 @@
-//use of if and else :
+//use of if and else 1 :
 
 //const age = 12;
-const age = 8;
+//const age = 8;
 
-if (age>10) {
-    console.log("go to home");
-}
-else {
-    console.log("not go to home");
-}
+//if (age>10) {
+// //console.log("go to home");
+//}
+//else {
+    //console.log("not go to home");
+//}
+
+//use of if and else 2 :
+
+//const age = 20;
+//const age = 15;
+//const age = 10;
+
+
+//if (age>=18) {
+    //console.log("every ride");
+//}
+//else if (age>=13) {
+    //console.log("3 ride");
+//}
+//else {
+    //console.log("no ride");
+// }
+
+//use of if and else 3 :
+
+//let age = 17;
+//let withAdult = false;
+
+//if (age >= 15) {
+    //console.log("apni malia dhukte parben");
+
+    //if (withAdult) {
+        //console.log("voutik bari entry");
+    //} else {
+        //console.log("no entry voutik bari");
+    //}
+
+//} else {
+    //console.log("no entry mala");

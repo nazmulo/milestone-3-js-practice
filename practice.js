8672d7766dd369996da60aa068a67d3397a85b0a
nazmulo <itsnaazmul@gmail.com>
2026-08-16 12:06:34 +0600

    practice the parse.int, parse.flote and total.tofixed use case

--- practice.js
+++ practice.js
@@ -1,8 +1,25 @@
 let weight = 58;
-console.log(typeof weight);
+//console.log(typeof weight);
 
 let country = "Canada";
-console.log(typeof country);
+//console.log(typeof country);
 
 let rich = true;
-console.log(typeof rich);
+//console.log(typeof rich);
+
+//parse.int/parse.float-use:
+
+let orangePrice = 20;
+let grapePrice = parseInt('10');
+console.log(orangePrice+grapePrice);
+
+let guavaPrice = 10;
+let applePrice = parseFloat(0.2);
+console.log(applePrice+grapePrice);
+
+let first = 0.1;
+let second = 0.2;
+var total = (first+second);
+console.log(total);
+
+console.log(total.toFixed(2));

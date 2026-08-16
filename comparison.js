8f80ec193d9e4ba482898e045892f58c0145586b
nazmulo <itsnaazmul@gmail.com>
2026-08-16 23:17:39 +0600

    comparison1

--- comparison.js
+++ comparison.js
@@ -1,5 +1,12 @@
-console.log(5>10);
-console.log(5<10);
-console.log(5==10);
-console.log(10===10);
-console.log(15>=10);
+//console.log(5>10);
+//console.log(5<10);
+//console.log(5==10);
+//console.log(10===10);
+//console.log(15>=10);
+
+let guava = 40;
+let apple = 250;
+console.log (guava>apple);
+console.log (guava<apple);
+console.log (5>=10);
+console.log (5<=10);

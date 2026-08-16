fc863bac12343c4c75a68f5e3ae6fdeb7140094c
nazmulo <itsnaazmul@gmail.com>
2026-08-16 15:15:27 +0600

    string

--- operation.js
+++ operation.js
@@ -5,5 +5,33 @@
   //let totalPrice = (onionPrice+eggPrice);
   //console.log(totalPrice);
 
-  let totalPrice = (onionPrice-eggPrice);
-  console.log(totalPrice);
+  //let totalPrice = (onionPrice-eggPrice);
+  //console.log(totalPrice);
+
+  //let totalPrice = (onionPrice*eggPrice);
+  //console.log(totalPrice);
+
+  //let totalPrice = (onionPrice/eggPrice);
+  //console.log(totalPrice);
+
+  //operation in shortcut:
+
+  //let price = 3500;
+  //price-= 1000;
+  //console.log(price);
+
+  //let price = 35;
+  //price+= 10;
+  //console.log(price);
+
+  
+  //let price = 30;
+  //price*= 3;
+  //console.log(price);
+
+  //let price = 30;
+  //price/= 3;
+  //console.log(price);
+
+  
+
---
+++ string-operation.js
@@ -0,0 +1,15 @@
+//string operation:
+
+  let first = "Mobarok";
+  let second = "Tobarok";
+  let fullname = (first+second);
+  //console.log(fullname);
+
+  //string use case :
+
+  //console.log(40+2);
+  //console.log(40+'2');
+  //console.log(40-'20');
+  //console.log('50'-20);
+  //console.log(40*'10');
+  //onsole.log(40/'10');

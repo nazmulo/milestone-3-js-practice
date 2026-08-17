c4c21269243ddc5f1766a9438b237dfc656ce58a
nazmulo <itsnaazmul@gmail.com>
2026-08-17 23:35:46 +0600

    use the nested and ternery

--- nester.js
+++ nester.js
@@ -1,20 +1,52 @@
 //nested:
 
-const money = -1;
+//const money = -1;
+//
+//if (money > 300) {
+//    console.log("bro-rich");
+//}
+//else {
+//    if (money > 100) {
+//        console.log("toi-modhobitto");
+//    }
+//    else {
+//        if (money > 0) {
+//            console.log("murikha");
+//        }
+//        else {
+//            console.log("toi ammer bondhu nna");
+//        }
+//    }
+//}
 
-if (money > 300) {
-    console.log("bro-rich");
-}
-else {
-    if (money > 100) {
-        console.log("toi-modhobitto");
-    }
-    else {
-        if (money > 0) {
-            console.log("murikha");
-        }
-        else {
-            console.log("toi ammer bondhu nna");
-        }
-    }
-}
+//ternery:
+
+//const age = 20;
+//if (age>=18) {
+//    console.log("you can vote");
+//}
+//else {
+//    console.log("gumia thako");
+//}
+
+//internery:
+
+//age>=18 ? console.log("you can vote") : console.log("gumia jaw");
+
+//nested ternery:
+
+//if (isLeader===true) {
+//    if (price>1000) {
+//        price=price/2;
+//    }
+//    else{
+//        price=0;
+//    }
+//}
+//else{
+//    price=price+10;
+//}
+//
+//in ternery:
+
+//price = isleader===true ? price>1000 ? price/2 : 0 : price+1000;

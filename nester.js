4b777fcfc7315789c153268a8fea2f0faa0081e1
nazmulo <itsnaazmul@gmail.com>
2026-08-17 23:16:05 +0600

    nested comparison

---
+++ nester.js
@@ -0,0 +1,20 @@
+//nested:
+
+const money = -1;
+
+if (money > 300) {
+    console.log("bro-rich");
+}
+else {
+    if (money > 100) {
+        console.log("toi-modhobitto");
+    }
+    else {
+        if (money > 0) {
+            console.log("murikha");
+        }
+        else {
+            console.log("toi ammer bondhu nna");
+        }
+    }
+}

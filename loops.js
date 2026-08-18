9731b1576d10bbeb80b46362cfec5d786b1b6465
nazmulo <itsnaazmul@gmail.com>
2026-08-18 13:09:32 +0600

    loop sum

---
+++ loops.js
@@ -0,0 +1,33 @@
+//loops:
+//const numbers = [12,48,45,63,21];
+//for (const num of numbers) {
+//    console.log(num);
+//}
+
+//const fruits = ["orange","apple","banana","jack-fruit","water-melon"];
+//for (const fruit of fruits) {
+//    console.log(fruit);
+//}
+
+//while loop :
+
+//let num = 1;
+//while (num<=10) {
+//    console.log(num);
+//    num++;
+//}
+
+//let num = 50;
+//while (num<=100) {
+//    console.log(num);
+//    num++;
+//}
+
+let num = 1;
+let sum = 0;
+while (num<=10) {
+    console.log(num);
+    sum = sum+num;
+    console.log("sum:", sum);
+    num++;
+}

560691e1b62f63c2344c50bb6036ecb40b64d633
nazmulo <itsnaazmul@gmail.com>
2026-08-18 10:17:23 +0600

    concat

---
+++ array.js
@@ -0,0 +1,54 @@
+//let fruits = [1,2,3,4,5,6,7];
+//console.log(fruits [1]);
+//console.log(fruits.length);
+//fruits[1] = "lichi";
+//console.log(fruits); 
+
+//const numbers = [1,2,3,4,5,6,7,8];
+//console.log(numbers[3]);
+
+//good = numbers [3];
+
+//console.log(good);
+
+//use push and pop:
+
+//const numbers = [12,87, 98, 45];
+//numbers.push(23);
+//console.log(numbers);
+
+//numbers.pop();
+//console.log(numbers);
+
+//use of shift and unshift:
+
+//numbers.shift();
+//console.log(numbers);
+
+//numbers.unshift(57);
+//console.log(numbers);
+
+//includes :
+
+//const friends = [1,2,3,4,5,6,7,8];
+//console.log(friends.includes(3));
+
+//if (friends.includes(10)) {
+//    console.log("party");
+//}
+//else {
+//    console.log("not food");
+//}
+
+//join use in array:
+
+//const apps = ["instagram", "facebook", "snapchat", "X", "youtube"];
+
+//console.log(apps.join("|"));
+
+//use of concat:
+
+let array1 = ["apple","goggle","openai"];
+let array2 = ["LV","MK","HV"];
+
+console.log(array1.concat(array2));

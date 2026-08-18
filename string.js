a78a514cdd89219663d925a76d3dbbe8c8a99200
nazmulo <itsnaazmul@gmail.com>
2026-08-18 22:16:06 +0600

    string use-case

---
+++ string.js
@@ -0,0 +1,43 @@
+//const capital = "Dhaka";
+//console.log(capital.length);
+
+//const capital = "Dhaka_";
+//console.log(capital.length);
+
+//string use-case:
+
+//const school = "RAJ UK Uttara Model School";
+//
+//console.log(school.toLowerCase());
+
+//const school = "RAJ UK Uttara Model School";
+//
+//console.log(school.toUpperCase());
+
+//mass use case:
+
+//const subject = "Chemistry";
+//
+//const book = "ChemIsTry";
+//
+//if (subject.toLowerCase() === book.toLowerCase()) {
+//    console.log("reading book");
+//} 
+//
+//else {
+//    console.log("vatiupre");
+//}
+
+//2 space thakle:
+
+const drink = "water";
+
+const liquid = "   water";
+
+if (drink.trim() === liquid.trim()) {
+    console.log("good");
+} 
+
+else {
+    console.log("bad");
+}

576efb9b00c59d33bc93a1f6277f80b774992da1
nazmulo <itsnaazmul@gmail.com>
2026-08-18 13:21:20 +0600

    even num loops

--- loops.js
+++ loops.js
@@ -23,11 +23,22 @@
 //    num++;
 //}
 
-let num = 1;
-let sum = 0;
-while (num<=10) {
-    console.log(num);
-    sum = sum+num;
-    console.log("sum:", sum);
-    num++;
-}
+//let num = 1;
+//let sum = 0;
+//while (num<=10) {
+//    console.log(num);
+//    sum = sum+num;
+//    console.log("sum:", sum);
+//    num++;
+//}
+
+//even number:
+
+//let num = 1;
+//while (num<=10){
+//    if (num%2===0){
+//        console.log("even number:", num);
+//    }
+//    num++;
+//
+//}

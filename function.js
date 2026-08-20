6fb6a341ad2e2aff7f4b6cc6a2722ef4809aeccf
nazmulo <itsnaazmul@gmail.com>
2026-08-20 16:41:19 +0600

    square function

---
+++ function.js
@@ -0,0 +1,21 @@
+//structure:
+
+//function sum () {
+//    console.log(sum);
+//}
+//sum();
+
+//function sum (x,y) {
+//    const result = x+y;
+//    return result;
+//} 
+//const total = sum (5,3);
+//console.log(total);
+
+//function square(number) {
+//    const borgo = number * number;
+//
+//    return borgo;
+//}
+//
+//console.log(square(5));

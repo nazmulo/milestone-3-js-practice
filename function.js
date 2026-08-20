541a0dd72134b6ff211307612514cdded9166d4d
nazmulo <itsnaazmul@gmail.com>
2026-08-20 16:51:19 +0600

    age diff function

--- function.js
+++ function.js
@@ -20,9 +20,18 @@
 //
 //console.log(square(5));
 
-function add (num1,num2) {
-    const sum = (num1+num2);
-    console.log(sum);
-}
+//function add (num1,num2) {
+//    const sum = (num1+num2);
+//    console.log(sum);
+//}
+//
+//add (5,71);
 
-add (5,71);
+//function diff (num1,num2) {
+//    const dif = num1-num2;
+//    console.log(dif);
+//}
+//
+//const fatherAge = 40;
+//const myAge = 10;
+//diff(fatherAge,myAge);

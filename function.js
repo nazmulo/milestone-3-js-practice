ac4f64a1f75bbeda503f2e50dbd9a7d141d51f54
nazmulo <itsnaazmul@gmail.com>
2026-08-21 14:43:27 +0600

    multiply function

--- function.js
+++ function.js
@@ -35,3 +35,10 @@
 //const fatherAge = 40;
 //const myAge = 10;
 //diff(fatherAge,myAge);
+
+//function tenTimes (number){
+//    const result = number*10;
+//    return result;
+//}
+//const output = tenTimes(5);
+//console.log(output);

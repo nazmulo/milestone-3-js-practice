5c804781467f086eb17c4f65b689e39af96bec7a
nazmulo <itsnaazmul@gmail.com>
2026-08-21 14:55:19 +0600

    big size function

--- function.js
+++ function.js
@@ -58,3 +58,13 @@
 //}
 //const bill = add (75,6);
 //console.log (bill);
+
+function doMath (num1,num2) {
+    const sum = (num1 + num2);
+    const diff = (num1-num2);
+    const multiply = sum*diff;
+    const result = multiply/2;
+    return result;
+}
+const result = doMath(10,5);
+console.log(result);

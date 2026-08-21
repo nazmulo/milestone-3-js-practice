61dc741201b790799e28eafa43065f4296788402
nazmulo <itsnaazmul@gmail.com>
2026-08-21 14:59:22 +0600

    conditional return function

--- function.js
+++ function.js
@@ -59,12 +59,25 @@
 //const bill = add (75,6);
 //console.log (bill);
 
-function doMath (num1,num2) {
-    const sum = (num1 + num2);
-    const diff = (num1-num2);
-    const multiply = sum*diff;
-    const result = multiply/2;
-    return result;
+//function doMath (num1,num2) {
+//    const sum = (num1 + num2);
+//    const diff = (num1-num2);
+//    const multiply = sum*diff;
+//    const result = multiply/2;
+//    return result;
+//}
+//const result = doMath(10,5);
+//console.log(result);
+
+//conditional return function:
+function isEven(number){
+    if(number%2 === 0){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
-const result = doMath(10,5);
-console.log(result);
+
+console.log(isEven(5));
+console.log(isEven(10));
